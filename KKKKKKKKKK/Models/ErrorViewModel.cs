f9d24527ce24c7ca6d96ba4257cb24b1d3fcede0
DaFontePvP <131710027+DaFontePvP@users.noreply.github.com>
2023-05-16 15:35:40 -0300

    att
    ...

---
+++ KKKKKKKKKK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KKKKKKKKKK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
